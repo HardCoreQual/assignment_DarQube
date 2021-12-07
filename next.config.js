0bef0f8da7474a1e9d9182168ab4ec34226b5275
hardcorequal <hardcorequal@gmail.com>
2021-12-07 18:10:43 +0200

    feat: add list of image providers

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['static.seekingalpha.com'],
+    // TODO: require copy all images on own hosting, for not lost, and next-js require list of hostings in next.config, it's need be predefined
+    domains: [
+      'static.seekingalpha.com',
+      'www.nasdaq.com',
+      'images.mktw.net',
+      'images.barrons.com',
+      'cdn.benzinga.com',
+      'images.wsj.net',
+      'si.wsj.net',
+      'static3.seekingalpha.com',
+      's.wsj.net'
+    ],
   },
 }
