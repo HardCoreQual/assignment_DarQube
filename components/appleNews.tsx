8c2f926f9f440ad6ec5ffc336fcbe1bdfd8acb90
hardcorequal <hardcorequal@gmail.com>
2021-12-08 19:33:40 +0200

    optimization: use React.memo for not re-rendering latest post

--- components/appleNews.tsx
+++ components/appleNews.tsx
@@ -1,10 +1,9 @@
 import {AppleOneNewsType} from "types/appleNews";
 import {Navbar} from "components/navbar";
 import {OneNews} from "components/oneNews";
-import {useAppDispatch} from "../store/store";
-import {newsActions, useNewsSelector} from "../store/news";
-import {useMemo} from "react";
-import styled from "styled-components";
+import {useNewsSelector} from "../store/news";
+import React, {useMemo} from "react";
+import {NewsList, SpaceBetween} from "components/newsList";
 
 export type AppleNewsProps = {
   news: AppleOneNewsType[]
@@ -20,7 +19,7 @@ export const AppleNews = () => {
   `}>
     <div css={`height: calc(100% - 80px)`}>
       <Navbar />
-      <AppleNewsList />
+      <AppleNewsDataBoard />
     </div>
   </div>
 }
@@ -29,13 +28,14 @@ function searchInText(text: string, search: string) {
   return text.toLowerCase().includes(search.toLowerCase());
 }
 
-const AppleNewsList = () => {
+const MemoOneNewsComponent = React.memo(OneNews);
+
+const AppleNewsDataBoard = () => {
   const news = useNewsSelector((state) => state.news);
   const isNewsMenu = useNewsSelector((state) => state.selectedMenu === 'news');
   const bookmarkIds = useNewsSelector((state) => state.bookmarkIds);
   const searchKeyword = useNewsSelector(state => state.search);
   const page = useNewsSelector((state) => state.page);
-  const dispatch = useAppDispatch();
 
   const menuNews = isNewsMenu ? news.slice(1) : news.filter((e) => bookmarkIds.includes(e.id));
 
@@ -50,63 +50,12 @@ const AppleNewsList = () => {
     {isNewsMenu && news.length && (
       <div>
         <div css={`height: 100%`}>
-          <OneNews oneNews={news[0]} isMain={true} />
+          <MemoOneNewsComponent oneNews={news[0]} isMain={true} />
         </div>
       </div>
     )}
-    <div css={`width: calc(100% - 496px)`}>
-      {showNews.slice(offset, offset + pageLimit).map(e => (
-        <div key={e.id} css={`width: ${33.33}%; display: inline-block`}>
-          <OneNews oneNews={e} isMain={false} />
-        </div>
-      ))}
 
-      <SpaceBetween css={`margin-right: 18px;`}>
-        <div css={`color: #fff`}>
-          <span>
-            {showNews.length === 0 ? 0 : <>
-              {page * pageLimit + 1}-{page * pageLimit + pageLimit}
-            </>}
-          </span>
-          <span css={`opacity: 0.25`}> of {showNews.length}</span>
-        </div>
-
-        <div>
-          {page > 0 && <Button onClick={() => dispatch(newsActions.previousPage())}>Previous</Button>}
-          {page < (showNews.length /pageLimit - 1) && <Button onClick={() => dispatch(newsActions.nextPage())}>Next</Button>}
-        </div>
-      </SpaceBetween>
-    </div>
+    <NewsList offset={offset} page={page} showNews={showNews} pageLimit={pageLimit} />
   </SpaceBetween>
 }
 
-const SpaceBetween = styled.div`
-  display: flex;
-  justify-content: space-between;
-`
-
-const Button = styled.div`
-  position: static;
-  left: 28.02%;
-  right: 28.02%;
-  top: 28%;
-  bottom: 28%;
-  font-family: Ubuntu;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 10px;
-  line-height: 11px;
-  text-transform: uppercase;
-  color: #FFFFFF;
-  flex: none;
-  order: 0;
-  flex-grow: 0;
-  display: inline-block;
-  padding: 7px 32px;
-  letter-spacing: 0em;
-  text-align: left;
-  background: #3C3C3C;
-  border-radius: 60px;
-  margin-left: 10px;
-  cursor: pointer;
-`;
---
+++ components/newsList.tsx
@@ -0,0 +1,68 @@
+import {AppleOneNewsType} from "types/appleNews";
+import {useAppDispatch} from "../store/store";
+import {OneNews} from "components/oneNews";
+import {newsActions} from "../store/news";
+import React from "react";
+import styled from "styled-components";
+
+export const NewsList = ({showNews, offset, pageLimit, page}: {
+  offset: number,
+  pageLimit: number,
+  page: number,
+  showNews: AppleOneNewsType[]
+}) => {
+  const dispatch = useAppDispatch();
+
+  return <div css={`width: calc(100% - 496px)`}>
+    {showNews.slice(offset, offset + pageLimit).map(e => (
+      <div key={e.id} css={`width: ${33.33}%; display: inline-block`}>
+        <OneNews oneNews={e} isMain={false}/>
+      </div>
+    ))}
+
+    <SpaceBetween css={`margin-right: 18px;`}>
+      <div css={`color: #fff`}>
+          <span>
+            {showNews.length === 0 ? 0 : <>
+              {page * pageLimit + 1}-{page * pageLimit + pageLimit}
+            </>}
+          </span>
+        <span css={`opacity: 0.25`}> of {showNews.length}</span>
+      </div>
+
+      <div>
+        {page > 0 && <Button onClick={() => dispatch(newsActions.previousPage())}>Previous</Button>}
+        {page < (showNews.length / pageLimit - 1) && <Button onClick={() => dispatch(newsActions.nextPage())}>Next</Button>}
+      </div>
+    </SpaceBetween>
+  </div>
+}
+export const SpaceBetween = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+const Button = styled.div`
+  position: static;
+  left: 28.02%;
+  right: 28.02%;
+  top: 28%;
+  bottom: 28%;
+  font-family: Ubuntu;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 10px;
+  line-height: 11px;
+  text-transform: uppercase;
+  color: #FFFFFF;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+  display: inline-block;
+  padding: 7px 32px;
+  letter-spacing: 0em;
+  text-align: left;
+  background: #3C3C3C;
+  border-radius: 60px;
+  margin-left: 10px;
+  cursor: pointer;
+`;
